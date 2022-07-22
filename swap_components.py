dd5781d886ef8e6715f30eab6de87ff277b20392
JarrettR <jrainier@gmail.com>
2022-07-22 11:20:33 -0700

    Initial KiCad 5 version

---
+++ swap_components.py
@@ -0,0 +1,50 @@
+"""
+Swaps the location of two selected components
+"""
+import pcbnew
+
+class ComponentSwap(pcbnew.ActionPlugin):
+    """
+    Select two (or more!) components and press the button
+    """
+    def defaults(self):
+        self.name = "Swap the location of components"
+        self.category = "Modify PCB"
+        self.description = "Swap the coordinates of selected components"
+        self.show_toolbar_button = True
+
+    def Run(self):
+        pcb = pcbnew.GetBoard()
+        
+        #
+        selectedMods = []
+        for mod in pcb.GetModules():
+            if mod.IsSelected():
+                selectedMods.append(mod)
+                
+        length = len(selectedMods)
+        newPos = []
+        
+        for i in range(length):
+            pos = selectedMods[i].GetPosition()
+            orient = selectedMods[i].GetOrientation()
+            layer = selectedMods[i].GetLayer()
+            flip = selectedMods[i].IsFlipped()
+            param = { 'pos': pos, 'orient': orient, 'layer': layer, 'flip': flip }
+            newPos.append(param)
+            
+        newPos = newPos[1:] + newPos[:1]
+            
+            
+        for i in range(length):
+            selectedMods[i].SetPosition(newPos[i]['pos'])
+            selectedMods[i].SetOrientation(newPos[i]['orient'])
+            selectedMods[i].SetLayer(newPos[i]['layer'])
+            # if selectedMods[i].IsFlipped() && !newPos[i]['flip']
+            
+        pcbnew.Refresh()
+            
+                
+
+
+ComponentSwap().register()
